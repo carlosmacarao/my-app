59b39d8eb8688ba7b4cba67ecba3bd2ac65f42fa
Carlos Macarao <82442020+carlosmacarao@users.noreply.github.com>
2024-03-28 09:11:40 +0200

    initial commit

---
+++ src/pages/Home/index.js
@@ -0,0 +1,21 @@
+
+import { StyleSheet, Text, View } from 'react-native';
+
+import Header from '../../components/Header';
+
+export default function Home() { 
+  return (
+    <View style={styles.container}>
+        <Header name="Carlos Macarão" />
+      <Text>Página Home!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+});
+ 
